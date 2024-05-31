29ee6dc688c21ee2e7b4ccc795f3e51c0e8be78c
sarim <58489447+sarim-hk@users.noreply.github.com>
2024-05-31 17:12:09 +0100

    Add project files.

---
+++ PlaytestPlugin/PlaytestPlugin.cs
@@ -0,0 +1,15 @@
+﻿using CounterStrikeSharp.API.Core;
+
+namespace PlaytestPlugin
+{
+    public class PlaytestPlugin : BasePlugin
+    {
+        public override string ModuleName => "PlaytestPlugin";
+        public override string ModuleVersion => "0.0.1";
+
+        public override void Load(bool hotReload)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
