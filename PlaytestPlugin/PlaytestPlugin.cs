e5bf16c2ae28412b24a4298b494b05834088c01f
sarim <58489447+sarim-hk@users.noreply.github.com>
2024-06-01 01:25:17 +0100

    partial class plugin suffixed printing hello world

--- PlaytestPlugin/PlaytestPlugin.cs
+++ PlaytestPlugin/PlaytestPlugin.cs
@@ -2,14 +2,15 @@
 
 namespace PlaytestPlugin
 {
-    public class PlaytestPlugin : BasePlugin
+    public partial class PlaytestPlugin : BasePlugin
     {
         public override string ModuleName => "PlaytestPlugin";
         public override string ModuleVersion => "0.0.1";
 
         public override void Load(bool hotReload)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("[PlaytestPlugin] Hello World!");
         }
     }
+
 }
