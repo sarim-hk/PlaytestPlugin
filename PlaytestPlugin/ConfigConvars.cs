84bc067e0b48d80256e52aaf9db897922a1a16e9
sarim <58489447+sarim-hk@users.noreply.github.com>
2024-06-02 12:45:22 +0100

    first commit

---
+++ PlaytestPlugin/ConfigConvars.cs
@@ -0,0 +1,11 @@
+﻿using CounterStrikeSharp.API.Modules.Cvars;
+using Microsoft.Extensions.Logging;
+
+namespace PlaytestPlugin
+{
+    public partial class PlaytestPlugin
+    {
+        public FakeConVar<bool> demoRecordingEnabled = new("pp_record_demos", "Should demos be recorded? Default: true", true);
+        public FakeConVar<string> demoUploadEndpoint = new("pp_demo_upload_endpoint", "The endpoint to which demos will be uploaded to.", string.Empty);
+    }
+}
